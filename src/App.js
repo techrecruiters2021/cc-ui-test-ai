5644ea73e59b46fd7657604082ac3d6667ee6d43
techrecruiters2021 <techrecruiters2021@gmail.com>
2021-09-28 13:43:23 -0300

    add paymentspageload event

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
+import ReactGA from 'react-ga';
 import CreditCardForm from "./components/CreditCardForm";
 
 function App() {
+  useEffect(() => {
+    ReactGA.event({
+      category: 'Conversions',
+      action: 'PaymentsPageLoad',
+      value: 1,
+      nonInteraction: true
+    });
+  }, [])
   return (
     <div className="App">
       <CreditCardForm />
